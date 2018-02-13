fc12ab0312d8fec7e09dc3f84798e4a663e5d761
Ryan Britton <rpbritton@outlook.com>
2018-02-13 01:04:01 -0500

    works (note: in feet, poorly implemented as always)

---
+++ PowerUp/src/Subsystems/Ultrasonic.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <WPILib.h>
+//#include "../Commands/RangeReadout.h"
+#include "Commands/Subsystem.h"
+
+
+class UltrasonicSensor: public frc::Subsystem
+{
+	public:
+		UltrasonicSensor(int analogChannel, int serialChannel, int onChannel);
+////       analogChannel = 1, serialChannel = 5, onChannel = 1
+
+		double GetRangeInInches() const;
+
+	private:
+
+		const float kMinVoltage;
+		const float kVoltageRange;
+		const float kMinDistance;
+		const float kDistanceRange;
+		mutable AnalogInput m_distanceChannel;
+
+		DigitalOutput m_serialMode;
+
+		DigitalOutput m_sensorOn;
+};
+
+
+
