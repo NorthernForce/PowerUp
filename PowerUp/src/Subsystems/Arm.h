6d964df1bb52fb08eeec9f5c93bbc78a96ad89f9
Joe Gaudet <gaudetja@gmail.com>
2018-02-20 21:49:58 -0500

    Change some PID tuning on the arm

--- PowerUp/src/Subsystems/Arm.h
+++ PowerUp/src/Subsystems/Arm.h
@@ -35,11 +35,11 @@ private:
 	constexpr static double sensorUnitsPerRev = 12 * 4; // The TalonSRX counts 4 edges per encoder count, the quadrature encoder has 12 counts per revolution
 	constexpr static double maxSensorUnitsPer100ms = sensorUnitsPerRev * maxSpeedRPM / 60 / 10;
 	constexpr static double feedForwardGain = 1023 / maxSensorUnitsPer100ms;
-	constexpr static double pGainPower = 0.1;
-	constexpr static double pGainError = 15;
+	constexpr static double pGainPower = 0.2;
+	constexpr static double pGainError = 10;
 	constexpr static double pGain = (1023 * pGainPower) / pGainError;
-	constexpr static double iGain = 0.01;
-	constexpr static double dGain = pGain / 10;
+	constexpr static double iGain = 0.08;
+	constexpr static double dGain = 0;//-1 * pGain / 10;
 	constexpr static double timeToMaxSpeed = 2.5;
 	constexpr static int slotIdx = 0;
 	constexpr static int pidIdx = 0;
