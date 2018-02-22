b200685a42f46243745991843696d9fb5718ba60
Ryan Britton <rpbritton@outlook.com>
2018-02-22 18:16:22 -0500

    just need to switch to master

---
+++ PowerUp/src/Commands/AutonomousScoreScale.cpp
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "AutonomousScoreScale.h"
+
+AutonomousScoreScale::AutonomousScoreScale() {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+}
+
+// Called just before this Command runs the first time
+void AutonomousScoreScale::Initialize() {
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void AutonomousScoreScale::Execute() {
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool AutonomousScoreScale::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void AutonomousScoreScale::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void AutonomousScoreScale::Interrupted() {
+
+}
