a6e48a286b7bd525ff21f2389ec43e43eafb4728
Stephen Nutt <stephennutt23@gmail.com>
2018-02-25 15:13:56 -0500

    Tuned the arm

--- PowerUp/src/Subsystems/Arm.h
+++ PowerUp/src/Subsystems/Arm.h
@@ -33,7 +33,7 @@ private:
 	constexpr static double freeSpeedRPM = 18730;
 	constexpr static double maxSpeedRPM = freeSpeedRPM * 0.80;
 	constexpr static double sensorUnitsPerRev = 12 * 4; // The TalonSRX counts 4 edges per encoder count, the quadrature encoder has 12 counts per revolution
-	constexpr static double maxSensorUnitsPer100ms = sensorUnitsPerRev * maxSpeedRPM / 60 / 10;
+	constexpr static double maxSensorUnitsPer100ms = sensorUnitsPerRev * maxSpeedRPM * 0.3 / 60 / 10;
 	constexpr static double feedForwardGain = 1023 / maxSensorUnitsPer100ms;
 	constexpr static double pGainPower = 0.2;
 	constexpr static double pGainError = 10;
