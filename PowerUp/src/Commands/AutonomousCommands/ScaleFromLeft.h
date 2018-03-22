93d894d520608abbfb2597ac1f058b4a4f8d1ffd
Shubhankar Naru <snaru2018@falmouthschools.org>
2018-03-22 15:29:20 -0700

    Implemented Code for Autonomous Scale from left side
    There is no code for if the scale is on the right side of the field yet.

--- PowerUp/src/Commands/AutonomousCommands/ScaleFromLeft.h
+++ PowerUp/src/Commands/AutonomousCommands/ScaleFromLeft.h
@@ -2,7 +2,9 @@
 #define SCALEFROMLEFT_H
 
 #include "Commands/CommandGroup.h"
+#include "DriverStation.h"
 #include "Utilities/FieldOrientation.h"
+#include "Utilities/RobotNavigation.h"
 #include "Commands/AutonomousDriveWithEncoders.h"
 #include "Commands/AutonomousTurnWithGyro.h"
 #include "Commands/PositionArm.h"
@@ -12,12 +14,24 @@ class ScaleFromLeft: public frc::CommandGroup
 public:
 	ScaleFromLeft() : frc::CommandGroup("Scale Autonomous From Left Side")
 	{
-		FieldOrientation::SetStartingRobotCoordinate(Position::Center, { FieldOrientation::FieldWidth / 2, FieldOrientation::StartCoordY, FieldOrientation::StartCoordHeading });
-		//TODO: this sequence only does left side scale from left side, add right side scale
+		const auto& ds = DriverStation::GetInstance();
+		const auto& message = ds.GetGameSpecificMessage();
+		const FieldOrientation orientation(message);
+		const RobotNavigation navigator(orientation);
+
+		//FieldOrientation::SetStartingRobotCoordinate(Position::Center, { FieldOrientation::FieldWidth / 2, FieldOrientation::StartCoordY, FieldOrientation::StartCoordHeading });
+		//if robot starts on left side and scale is on left side..
 		AddSequential(new AutonomousDriveWithEncoders(3.5, 0.8));
-		AddSequential(new AutonomousTurnWithGyro(-20, 0.6));
-		AddSequential(new PositionArm(PositionArm::Position::ScaleFront));
-		AddSequential(new AutonomousDriveWithEncoders(3.5, 0.8));
+		if(orientation.GetStartingRobotPos() == Position::Left && orientation.GetStartRobotPos() == orientation.GetScalePos()) {
+			AddSequential(new AutonomousTurnWithGyro(-20, 0.6));
+			AddSequential(new PositionArm(PositionArm::Position::ScaleFront));
+			AddSequential(new AutonomousDriveWithEncoders(3.5, 0.8));
+		} else if(orientation.GetStartingRobotPos() == Position::Left && orientation.GetStartingPos() != orientation.GetScalePos()) { //if robot starts on left side and scale is on right side..
+			//TODO: autonomous code for right-side-scale from left side
+		}
+			AddSequential(new AutonomousWait(500));
+			AddSequential(new OpenGripper());
+			AddSequential(new AutonomousWait(500));
 
 	}
 };
