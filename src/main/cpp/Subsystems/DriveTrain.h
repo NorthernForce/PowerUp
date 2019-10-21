d57133df295d286035481f9b429791506c89b626
Hunter-Pellerin <hpellerin2003@gmail.com>
2019-10-21 18:33:24 -0400

    can't find M_PI in <cmath> or <math.h> with custom definitions so I custom defined it

--- src/main/cpp/Subsystems/DriveTrain.h
+++ src/main/cpp/Subsystems/DriveTrain.h
@@ -8,9 +8,9 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
 #ifndef DRIVETRAIN_H
 #define DRIVETRAIN_H
+#define M_PI 3.14159265358979323846 // pi
 #include "Commands/Subsystem.h"
 #include "Utilities/MotionProfile.h"
 #include "Utilities/TalonTelemetry.h"
