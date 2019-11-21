2dafcef06ff96c444d6e30fcb7be5a546fc96d8b
Hunter-Pellerin <hpellerin2003@gmail.com>
2019-11-21 16:06:51 -0500

    commented out possibly broken gear shifting code

--- src/main/cpp/commands/ShiftGear.cpp
+++ src/main/cpp/commands/ShiftGear.cpp
@@ -1,37 +1,37 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// /*----------------------------------------------------------------------------*/
+// /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+// /* Open Source Software - may be modified and shared by FRC teams. The code   */
+// /* must be accompanied by the FIRST BSD license file in the root directory of */
+// /* the project.                                                               */
+// /*----------------------------------------------------------------------------*/
 
-#include "frc/DriverStation.h"
-#include "commands/ShiftGear.h"
-#include "Robot.h"
+// #include "frc/DriverStation.h"
+// #include "commands/ShiftGear.h"
+// #include "Robot.h"
 
-ShiftGear::ShiftGear(Gear gear) : Command("ShiftGear"), m_gear(gear)
-{
-  Requires(Robot::m_driveTrainShifter.get());
-  Requires(Robot::m_driveTrain.get());
-  strcpy(m_logMessage, m_gear == Gear::Low ? "Shift to low" : "Shift to high");
-}
+// ShiftGear::ShiftGear(Gear gear) : Command("ShiftGear"), m_gear(gear)
+// {
+//   Requires(Robot::m_driveTrainShifter.get());
+//   Requires(Robot::m_driveTrain.get());
+//   strcpy(m_logMessage, m_gear == Gear::Low ? "Shift to low" : "Shift to high");
+// }
 
-// Called just before this Command runs the first time
-void ShiftGear::Initialize()
-{
-  	frc::DriverStation::ReportWarning(m_logMessage);
-	Robot::m_driveTrainShifter->Shift(m_gear);
-}
+// // Called just before this Command runs the first time
+// void ShiftGear::Initialize()
+// {
+//   frc::DriverStation::ReportWarning(m_logMessage);
+// 	Robot::m_driveTrainShifter->Shift(m_gear);
+// }
 
-// Called repeatedly when this Command is scheduled to run
-void ShiftGear::Execute() {}
+// // Called repeatedly when this Command is scheduled to run
+// void ShiftGear::Execute() {}
 
-// Make this return true when this Command no longer needs to run execute()
-bool ShiftGear::IsFinished() { return Robot::m_driveTrainShifter->IsShiftDone(); }
+// // Make this return true when this Command no longer needs to run execute()
+// bool ShiftGear::IsFinished() { return Robot::m_driveTrainShifter->IsShiftDone(); }
 
-// Called once after isFinished returns true
-void ShiftGear::End() {}
+// // Called once after isFinished returns true
+// void ShiftGear::End() {}
 
-// Called when another command which requires one or more of the same
-// subsystems is scheduled to run
-void ShiftGear::Interrupted() {}
+// // Called when another command which requires one or more of the same
+// // subsystems is scheduled to run
+// void ShiftGear::Interrupted() {}
--- src/main/include/commands/ShiftGear.h
+++ src/main/include/commands/ShiftGear.h
@@ -1,25 +1,25 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// /*----------------------------------------------------------------------------*/
+// /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+// /* Open Source Software - may be modified and shared by FRC teams. The code   */
+// /* must be accompanied by the FIRST BSD license file in the root directory of */
+// /* the project.                                                               */
+// /*----------------------------------------------------------------------------*/
 
-#pragma once
+// #pragma once
 
-#include <frc/commands/Command.h>
-#include "subsystems/DriveTrainShifter.h"
+// #include <frc/commands/Command.h>
+// #include "subsystems/DriveTrainShifter.h"
 
-class ShiftGear : public frc::Command {
- public:
-  using Gear = DriveTrainShifter::Gear;
-  ShiftGear(Gear gear);
-  void Initialize() override;
-  void Execute() override;
-  bool IsFinished() override;
-  void End() override;
-  void Interrupted() override;
- private:
-  Gear m_gear;
-	char m_logMessage[32];
-};
+// class ShiftGear : public frc::Command {
+//  public:
+//   using Gear = DriveTrainShifter::Gear;
+//   ShiftGear(Gear gear);
+//   void Initialize() override;
+//   void Execute() override;
+//   bool IsFinished() override;
+//   void End() override;
+//   void Interrupted() override;
+//  private:
+//   Gear m_gear;
+// 	char m_logMessage[32];
+// };
